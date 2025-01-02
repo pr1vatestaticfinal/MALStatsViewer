e504f00552ad65a2c745c6e8f7166289775227e3
Michael <dasmemichael@gmail.com>
2025-01-02 13:59:22 -0800

    centered buttons and made them work, added sign out button

--- stats.js
+++ stats.js
@@ -5,18 +5,20 @@ const years = [2013, 2014, 2022];
 //when username is received, text is shown
 if (username == "username") {
     const welcome = document.querySelector(".welcome");
-    welcome.style.display = "block";
     welcome.textContent = "Welcome, " + username;
 }
 
 if (decades != null) {
-    document.getElementById("decades-toggle")
     const dropdown = document.getElementById("decades-choices");
     dropdown.innerHTML = "";
 
     for (let i = 0; i < decades.length; i++) {
         const button = document.createElement("button");
-        button.onclick = () => someFunction(decades[i]); //PLACEHOLDER FUNCTION FIX WHEN COOKIE DATA IS IMPLEMENTED
+        button.onclick = () => {
+            //someFunction(decades[i]); //PLACEHOLDER FUNCTION FIX WHEN COOKIE DATA IS IMPLEMENTED
+            dropdown.style.display = "none";
+            console.log("you have selected", decades[i]);
+        };
         button.textContent = decades[i] + "'s";
         dropdown.appendChild(button);
     }
@@ -30,20 +32,17 @@ toggleDecades.addEventListener("click", () => {
     decadesContent.style.display = isVisible ? "none" : "block";
 });
 
-document.addEventListener("click", (event) => {
-    if (!event.target.closest(".timeContainer")) {
-        decadesContent.style.display = "none";
-    }
-});
-
 if (years != null) {
-    document.getElementById("years-toggle")
     const dropdown = document.getElementById("years-choices");
     dropdown.innerHTML = "";
 
     for (let i = 0; i < years.length; i++) {
         const button = document.createElement("button");
-        button.onclick = () => someFunction(years[i]); //PLACEHOLDER FUNCTION FIX WHEN COOKIE DATA IS IMPLEMENTED
+        button.onclick = () => {
+            //someFunction(years[i]); //PLACEHOLDER FUNCTION FIX WHEN COOKIE DATA IS IMPLEMENTED
+            dropdown.style.display = "none";
+            console.log("you have selected", years[i]);
+        };
         button.textContent = years[i];
         dropdown.appendChild(button);
     }
@@ -55,10 +54,12 @@ const yearsContent = document.getElementById("years-choices");
 toggleYears.addEventListener("click", () => {
     const isVisible = yearsContent.style.display === "block";
     yearsContent.style.display = isVisible ? "none" : "block";
+    decadesContent.style.display = "none";
 });
 
 document.addEventListener("click", (event) => {
-    if (!event.target.closest(".timeContainer")) {
+    if (!event.target.closest("#timeContainer")) {
         yearsContent.style.display = "none";
+        decadesContent.style.display = "none";
     }
 });
